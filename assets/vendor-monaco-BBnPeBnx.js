7b5cd5840a77c6dc80eb47fd128314b5ef8aa2cd
emilyoram <emilyoramnz@gmail.com>
2026-08-25 23:35:58 +0000

    Deploying to gh-pages from @ camunda/web-demo-framework@d30915fc9bcd26409c97beb35e003c0e46fd1b6c 🚀

--- assets/vendor-monaco-BBnPeBnx.js
+++ assets/vendor-monaco-BBnPeBnx.js
@@ -1,4 +1,4 @@
-import{m as i}from"./MonacoEditor-BOEpRDyu.js";import"./index-ZRu63jPP.js";import"./vendor-react-9Ma26nY1.js";import"./vendor-design-system-B7LgQvIL.js";/*!-----------------------------------------------------------------------------
+import{m as i}from"./MonacoEditor-BVxAHVAL.js";import"./index-9jijqkk5.js";import"./vendor-react-9Ma26nY1.js";import"./vendor-design-system-B7LgQvIL.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.52.2(404545bded1df6ffa41ea0af4e8ddb219018c6c1)
  * Released under the MIT license
--- assets/vendor-monaco-BbQdLmWO.js
+++ assets/vendor-monaco-BbQdLmWO.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./vendor-monaco-BoTE4uOE.js";import"./MonacoEditor-BOEpRDyu.js";import"./index-ZRu63jPP.js";import"./vendor-react-9Ma26nY1.js";import"./vendor-design-system-B7LgQvIL.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./vendor-monaco-DNRBbhbl.js";import"./MonacoEditor-BVxAHVAL.js";import"./index-9jijqkk5.js";import"./vendor-react-9Ma26nY1.js";import"./vendor-design-system-B7LgQvIL.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.52.2(404545bded1df6ffa41ea0af4e8ddb219018c6c1)
  * Released under the MIT license
