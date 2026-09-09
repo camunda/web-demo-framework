5470543ea4c205df4b20f12380d9f78785872ea1
emilyoram <emilyoramnz@gmail.com>
2026-09-09 01:10:18 +0000

    Deploying to gh-pages from @ camunda/web-demo-framework@abc918b3fc98c0a51b6193f6d4448248e078b8f4 🚀

--- assets/vendor-monaco-DHog2jpk.js
+++ assets/vendor-monaco-DHog2jpk.js
@@ -1,4 +1,4 @@
-import{m as i}from"./MonacoEditor-BtpuKzfy.js";import"./index-Bewvjy08.js";import"./vendor-react-9Ma26nY1.js";import"./vendor-design-system-B2HQUYYn.js";/*!-----------------------------------------------------------------------------
+import{m as i}from"./MonacoEditor-DndvL9_5.js";import"./index-BrTkw6n7.js";import"./vendor-react-9Ma26nY1.js";import"./vendor-design-system-B2HQUYYn.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.52.2(404545bded1df6ffa41ea0af4e8ddb219018c6c1)
  * Released under the MIT license
