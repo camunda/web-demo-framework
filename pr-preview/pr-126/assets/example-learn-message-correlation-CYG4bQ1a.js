07c188c4521362d93cf787fe9fd786d0cff13544
emilyoram <emilyoram@users.noreply.github.com>
2026-09-11 03:34:29 +0000

    Deploy preview for PR 126 🛫

---
+++ pr-preview/pr-126/assets/example-learn-message-correlation-CYG4bQ1a.js
@@ -0,0 +1,76 @@
+import{e as n}from"./index-B_OdpJBd.js";import"./vendor-react-9Ma26nY1.js";import"./vendor-design-system-D_22Wa_7.js";const e=`<?xml version="1.0" encoding="UTF-8"?>\r
+<bpmn:definitions xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" xmlns:zeebe="http://camunda.org/schema/zeebe/1.0" xmlns:di="http://www.omg.org/spec/DD/20100524/DI" xmlns:modeler="http://camunda.org/schema/modeler/1.0" id="Definitions_learn_message_correlation" targetNamespace="http://bpmn.io/schema/bpmn" exporter="Camunda Modeler" exporterVersion="5.36.1" modeler:executionPlatform="Camunda Cloud" modeler:executionPlatformVersion="8.8.0">\r
+  <bpmn:message id="Message_ShipmentConfirmed" name="shipment-confirmed">\r
+    <bpmn:extensionElements>\r
+      <zeebe:subscription correlationKey="=orderId" />\r
+    </bpmn:extensionElements>\r
+  </bpmn:message>\r
+  <bpmn:process id="learn_message_correlation" name="Message catch event + correlation" isExecutable="true">\r
+    <bpmn:startEvent id="StartEvent_1" name="Order placed">\r
+      <bpmn:outgoing>Flow_to_wait</bpmn:outgoing>\r
+    </bpmn:startEvent>\r
+    <bpmn:intermediateCatchEvent id="Event_wait_for_shipment" name="Wait for shipment confirmed">\r
+      <bpmn:incoming>Flow_to_wait</bpmn:incoming>\r
+      <bpmn:outgoing>Flow_to_record</bpmn:outgoing>\r
+      <bpmn:messageEventDefinition id="MessageEventDefinition_1" messageRef="Message_ShipmentConfirmed" />\r
+    </bpmn:intermediateCatchEvent>\r
+    <bpmn:serviceTask id="Activity_record" name="Record confirmation">\r
+      <bpmn:extensionElements>\r
+        <zeebe:taskDefinition type="record-confirmation" />\r
+      </bpmn:extensionElements>\r
+      <bpmn:incoming>Flow_to_record</bpmn:incoming>\r
+      <bpmn:outgoing>Flow_to_end</bpmn:outgoing>\r
+    </bpmn:serviceTask>\r
+    <bpmn:endEvent id="Event_done" name="Order fulfilled">\r
+      <bpmn:incoming>Flow_to_end</bpmn:incoming>\r
+    </bpmn:endEvent>\r
+    <bpmn:sequenceFlow id="Flow_to_wait" sourceRef="StartEvent_1" targetRef="Event_wait_for_shipment" />\r
+    <bpmn:sequenceFlow id="Flow_to_record" sourceRef="Event_wait_for_shipment" targetRef="Activity_record" />\r
+    <bpmn:sequenceFlow id="Flow_to_end" sourceRef="Activity_record" targetRef="Event_done" />\r
+  </bpmn:process>\r
+  <bpmndi:BPMNDiagram id="BPMNDiagram_1">\r
+    <bpmndi:BPMNPlane id="BPMNPlane_1" bpmnElement="learn_message_correlation">\r
+      <bpmndi:BPMNShape id="StartEvent_1_di" bpmnElement="StartEvent_1">\r
+        <dc:Bounds x="182" y="212" width="36" height="36" />\r
+        <bpmndi:BPMNLabel>\r
+          <dc:Bounds x="160" y="255" width="80" height="14" />\r
+        </bpmndi:BPMNLabel>\r
+      </bpmndi:BPMNShape>\r
+      <bpmndi:BPMNShape id="Event_wait_for_shipment_di" bpmnElement="Event_wait_for_shipment">\r
+        <dc:Bounds x="270" y="212" width="36" height="36" />\r
+        <bpmndi:BPMNLabel>\r
+          <dc:Bounds x="248" y="255" width="80" height="27" />\r
+        </bpmndi:BPMNLabel>\r
+      </bpmndi:BPMNShape>\r
+      <bpmndi:BPMNShape id="Activity_record_di" bpmnElement="Activity_record">\r
+        <dc:Bounds x="380" y="190" width="100" height="80" />\r
+        <bpmndi:BPMNLabel />\r
+      </bpmndi:BPMNShape>\r
+      <bpmndi:BPMNShape id="Event_done_di" bpmnElement="Event_done">\r
+        <dc:Bounds x="542" y="212" width="36" height="36" />\r
+        <bpmndi:BPMNLabel>\r
+          <dc:Bounds x="520" y="255" width="80" height="27" />\r
+        </bpmndi:BPMNLabel>\r
+      </bpmndi:BPMNShape>\r
+      <bpmndi:BPMNEdge id="Flow_to_wait_di" bpmnElement="Flow_to_wait">\r
+        <di:waypoint x="218" y="230" />\r
+        <di:waypoint x="270" y="230" />\r
+      </bpmndi:BPMNEdge>\r
+      <bpmndi:BPMNEdge id="Flow_to_record_di" bpmnElement="Flow_to_record">\r
+        <di:waypoint x="306" y="230" />\r
+        <di:waypoint x="380" y="230" />\r
+      </bpmndi:BPMNEdge>\r
+      <bpmndi:BPMNEdge id="Flow_to_end_di" bpmnElement="Flow_to_end">\r
+        <di:waypoint x="480" y="230" />\r
+        <di:waypoint x="542" y="230" />\r
+      </bpmndi:BPMNEdge>\r
+    </bpmndi:BPMNPlane>\r
+  </bpmndi:BPMNDiagram>\r
+</bpmn:definitions>\r
+`,r=`async (job, { text, trace }) => {
+  const orderId = text("orderId", "unknown-order");
+
+  trace("shipment confirmed for " + orderId + " — recording it");
+
+  return { shipmentRecorded: true };
+}`,m={...n,bpmn:e,seed:{orderId:"ORD-42"},handlers:[{elementId:"Activity_record",standsInFor:"job worker — record-confirmation",source:r}]};export{m as default};
