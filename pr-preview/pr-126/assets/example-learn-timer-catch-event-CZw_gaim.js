4ceee006128f892ef135112c3dce365b59ff6555
emilyoram <emilyoram@users.noreply.github.com>
2026-09-11 02:44:48 +0000

    Deploy preview for PR 126 🛫

---
+++ pr-preview/pr-126/assets/example-learn-timer-catch-event-CZw_gaim.js
@@ -0,0 +1,72 @@
+import{i as n}from"./index-D4Sdm4ds.js";import"./vendor-react-9Ma26nY1.js";import"./vendor-design-system-D_22Wa_7.js";const e=`<?xml version="1.0" encoding="UTF-8"?>\r
+<bpmn:definitions xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" xmlns:zeebe="http://camunda.org/schema/zeebe/1.0" xmlns:di="http://www.omg.org/spec/DD/20100524/DI" xmlns:modeler="http://camunda.org/schema/modeler/1.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" id="Definitions_learn_timer_catch_event" targetNamespace="http://bpmn.io/schema/bpmn" exporter="Camunda Modeler" exporterVersion="5.36.1" modeler:executionPlatform="Camunda Cloud" modeler:executionPlatformVersion="8.8.0">\r
+  <bpmn:process id="learn_timer_catch_event" name="Timer catch event" isExecutable="true">\r
+    <bpmn:startEvent id="StartEvent_1" name="Reminder scheduled">\r
+      <bpmn:outgoing>Flow_to_timer</bpmn:outgoing>\r
+    </bpmn:startEvent>\r
+    <bpmn:intermediateCatchEvent id="Event_timer" name="Wait 3 seconds">\r
+      <bpmn:incoming>Flow_to_timer</bpmn:incoming>\r
+      <bpmn:outgoing>Flow_to_task</bpmn:outgoing>\r
+      <bpmn:timerEventDefinition id="TimerEventDefinition_1">\r
+        <bpmn:timeDuration xsi:type="bpmn:tFormalExpression">PT3S</bpmn:timeDuration>\r
+      </bpmn:timerEventDefinition>\r
+    </bpmn:intermediateCatchEvent>\r
+    <bpmn:serviceTask id="Activity_after_timer" name="Send the reminder">\r
+      <bpmn:extensionElements>\r
+        <zeebe:taskDefinition type="send-reminder" />\r
+      </bpmn:extensionElements>\r
+      <bpmn:incoming>Flow_to_task</bpmn:incoming>\r
+      <bpmn:outgoing>Flow_to_end</bpmn:outgoing>\r
+    </bpmn:serviceTask>\r
+    <bpmn:endEvent id="Event_done" name="Reminder sent">\r
+      <bpmn:incoming>Flow_to_end</bpmn:incoming>\r
+    </bpmn:endEvent>\r
+    <bpmn:sequenceFlow id="Flow_to_timer" sourceRef="StartEvent_1" targetRef="Event_timer" />\r
+    <bpmn:sequenceFlow id="Flow_to_task" sourceRef="Event_timer" targetRef="Activity_after_timer" />\r
+    <bpmn:sequenceFlow id="Flow_to_end" sourceRef="Activity_after_timer" targetRef="Event_done" />\r
+  </bpmn:process>\r
+  <bpmndi:BPMNDiagram id="BPMNDiagram_1">\r
+    <bpmndi:BPMNPlane id="BPMNPlane_1" bpmnElement="learn_timer_catch_event">\r
+      <bpmndi:BPMNShape id="StartEvent_1_di" bpmnElement="StartEvent_1">\r
+        <dc:Bounds x="182" y="212" width="36" height="36" />\r
+        <bpmndi:BPMNLabel>\r
+          <dc:Bounds x="160" y="255" width="80" height="27" />\r
+        </bpmndi:BPMNLabel>\r
+      </bpmndi:BPMNShape>\r
+      <bpmndi:BPMNShape id="Event_timer_di" bpmnElement="Event_timer">\r
+        <dc:Bounds x="272" y="212" width="36" height="36" />\r
+        <bpmndi:BPMNLabel>\r
+          <dc:Bounds x="252" y="255" width="80" height="27" />\r
+        </bpmndi:BPMNLabel>\r
+      </bpmndi:BPMNShape>\r
+      <bpmndi:BPMNShape id="Activity_after_timer_di" bpmnElement="Activity_after_timer">\r
+        <dc:Bounds x="370" y="190" width="100" height="80" />\r
+        <bpmndi:BPMNLabel />\r
+      </bpmndi:BPMNShape>\r
+      <bpmndi:BPMNShape id="Event_done_di" bpmnElement="Event_done">\r
+        <dc:Bounds x="532" y="212" width="36" height="36" />\r
+        <bpmndi:BPMNLabel>\r
+          <dc:Bounds x="510" y="255" width="80" height="27" />\r
+        </bpmndi:BPMNLabel>\r
+      </bpmndi:BPMNShape>\r
+      <bpmndi:BPMNEdge id="Flow_to_timer_di" bpmnElement="Flow_to_timer">\r
+        <di:waypoint x="218" y="230" />\r
+        <di:waypoint x="272" y="230" />\r
+      </bpmndi:BPMNEdge>\r
+      <bpmndi:BPMNEdge id="Flow_to_task_di" bpmnElement="Flow_to_task">\r
+        <di:waypoint x="308" y="230" />\r
+        <di:waypoint x="370" y="230" />\r
+      </bpmndi:BPMNEdge>\r
+      <bpmndi:BPMNEdge id="Flow_to_end_di" bpmnElement="Flow_to_end">\r
+        <di:waypoint x="470" y="230" />\r
+        <di:waypoint x="532" y="230" />\r
+      </bpmndi:BPMNEdge>\r
+    </bpmndi:BPMNPlane>\r
+  </bpmndi:BPMNDiagram>\r
+</bpmn:definitions>\r
+`,t=`async (job, { sleep, trace }) => {
+  trace("the timer fired — sending the reminder now");
+  await sleep(400);
+
+  return { reminderSent: true };
+}`,d={...n,bpmn:e,seed:{},handlers:[{elementId:"Activity_after_timer",standsInFor:"job worker — send-reminder",source:t}]};export{d as default};
