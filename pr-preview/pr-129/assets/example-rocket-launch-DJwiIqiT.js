4b02dc98839a3d2e8244f7386cd85414de23e9eb
emilyoram <emilyoram@users.noreply.github.com>
2026-09-17 01:36:33 +0000

    Deploy preview for PR 129 🛫

---
+++ pr-preview/pr-129/assets/example-rocket-launch-DJwiIqiT.js
@@ -0,0 +1,87 @@
+import{q as n}from"./index-CnclkpNJ.js";import"./vendor-react-9Ma26nY1.js";import"./vendor-design-system-jWpzD6a9.js";const e=`<?xml version="1.0" encoding="UTF-8"?>
+<bpmn:definitions xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" xmlns:zeebe="http://camunda.org/schema/zeebe/1.0" xmlns:di="http://www.omg.org/spec/DD/20100524/DI" xmlns:modeler="http://camunda.org/schema/modeler/1.0" id="Definitions_RocketLaunch" targetNamespace="http://bpmn.io/schema/bpmn" exporter="Camunda Modeler" exporterVersion="5.36.1" modeler:executionPlatform="Camunda Cloud" modeler:executionPlatformVersion="8.8.0">
+  <bpmn:process id="rocket-launch" name="Rocket Launch" isExecutable="true">
+    <bpmn:startEvent id="StartEvent_1" name="Launch sequence initiated">
+      <bpmn:outgoing>Flow_Start_Launch</bpmn:outgoing>
+    </bpmn:startEvent>
+    <bpmn:sequenceFlow id="Flow_Start_Launch" sourceRef="StartEvent_1" targetRef="Activity_LaunchRocket" />
+    <bpmn:serviceTask id="Activity_LaunchRocket" name="Launch rocket">
+      <bpmn:extensionElements>
+        <zeebe:taskDefinition type="launch-rocket" />
+      </bpmn:extensionElements>
+      <bpmn:incoming>Flow_Start_Launch</bpmn:incoming>
+      <bpmn:outgoing>Flow_Launch_End</bpmn:outgoing>
+    </bpmn:serviceTask>
+    <bpmn:endEvent id="EndEvent_MissionComplete" name="Mission complete">
+      <bpmn:incoming>Flow_Launch_End</bpmn:incoming>
+    </bpmn:endEvent>
+    <bpmn:sequenceFlow id="Flow_Launch_End" sourceRef="Activity_LaunchRocket" targetRef="EndEvent_MissionComplete" />
+    <bpmn:textAnnotation id="TextAnnotation_1">
+      <bpmn:text>The smallest possible example: one service task, based on 'camunda-8-get-started/1-rocket-launch'.</bpmn:text>
+    </bpmn:textAnnotation>
+    <bpmn:association id="Association_1" associationDirection="None" sourceRef="StartEvent_1" targetRef="TextAnnotation_1" />
+  </bpmn:process>
+  <bpmndi:BPMNDiagram id="BPMNDiagram_1">
+    <bpmndi:BPMNPlane id="BPMNPlane_1" bpmnElement="rocket-launch">
+      <bpmndi:BPMNShape id="StartEvent_1_di" bpmnElement="StartEvent_1">
+        <dc:Bounds x="182" y="212" width="36" height="36" />
+        <bpmndi:BPMNLabel>
+          <dc:Bounds x="150" y="255" width="100" height="27" />
+        </bpmndi:BPMNLabel>
+      </bpmndi:BPMNShape>
+      <bpmndi:BPMNShape id="Activity_LaunchRocket_di" bpmnElement="Activity_LaunchRocket">
+        <dc:Bounds x="270" y="190" width="100" height="80" />
+        <bpmndi:BPMNLabel />
+      </bpmndi:BPMNShape>
+      <bpmndi:BPMNShape id="EndEvent_MissionComplete_di" bpmnElement="EndEvent_MissionComplete">
+        <dc:Bounds x="432" y="212" width="36" height="36" />
+        <bpmndi:BPMNLabel>
+          <dc:Bounds x="410" y="255" width="80" height="14" />
+        </bpmndi:BPMNLabel>
+      </bpmndi:BPMNShape>
+      <bpmndi:BPMNEdge id="Flow_Start_Launch_di" bpmnElement="Flow_Start_Launch">
+        <di:waypoint x="218" y="230" />
+        <di:waypoint x="270" y="230" />
+      </bpmndi:BPMNEdge>
+      <bpmndi:BPMNEdge id="Flow_Launch_End_di" bpmnElement="Flow_Launch_End">
+        <di:waypoint x="370" y="230" />
+        <di:waypoint x="432" y="230" />
+      </bpmndi:BPMNEdge>
+      <bpmndi:BPMNEdge id="Association_1_di" bpmnElement="Association_1">
+        <di:waypoint x="214" y="219" />
+        <di:waypoint x="268" y="135" />
+      </bpmndi:BPMNEdge>
+      <bpmndi:BPMNShape id="TextAnnotation_1_di" bpmnElement="TextAnnotation_1">
+        <dc:Bounds x="240" y="80" width="230" height="55" />
+        <bpmndi:BPMNLabel />
+      </bpmndi:BPMNShape>
+    </bpmndi:BPMNPlane>
+  </bpmndi:BPMNDiagram>
+</bpmn:definitions>
+`,t=`async (job, { text, num, sleep, trace }) => {
+  // The starting variables arrive on 'job.variables'.
+  const missionName = text("missionName", "Apollo 11");
+  const destination = text("destination", "the Moon");
+  const fuelLevel = num("fuelLevel", 100);
+
+  // Stands in for a job worker on a real cluster calling out to mission
+  // control (or, more realistically, doing the actual work that the task name
+  // implies — here, running the launch sequence and burning fuel).
+  await sleep(600);
+
+  if (fuelLevel < 50) {
+    trace(\`\${missionName} scrubbed — not enough fuel (\${fuelLevel}%)\`);
+    // Whatever you return is merged onto the process instance.
+    return { missionStatus: "scrubbed", fuelLevel };
+  }
+
+  const fuelAfterLaunch = fuelLevel - 25;
+  trace(\`\${missionName} launched toward \${destination}\`);
+
+  return {
+    missionStatus: "launched",
+    destination,
+    fuelLevel: fuelAfterLaunch,
+    missionResult: \`Crew \${missionName} launched toward \${destination}! Fuel remaining: \${fuelAfterLaunch}%.\`,
+  };
+}`,m={...n,bpmn:e,seed:{missionName:"Apollo 11",destination:"the Moon",fuelLevel:100},scenarios:[{label:"Full tanks — launch succeeds",variables:{missionName:"Apollo 11",destination:"the Moon",fuelLevel:100}},{label:"Low fuel — mission scrubbed",variables:{missionName:"Apollo 13",destination:"the Moon",fuelLevel:30}}],handlers:[{elementId:"Activity_LaunchRocket",standsInFor:"job worker — launch-rocket",source:t}]};export{m as rocketLaunch};
